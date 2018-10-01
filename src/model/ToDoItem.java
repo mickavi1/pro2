202f28ef565b62fe0825bf8c89463998e0545da8
mickavi1 <mickavi1@uhk.cz>
2018-10-01 13:52:48 +0200

    Simple UI - Added new buttons, editing and values...

---
+++ src/model/ToDoItem.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ToDoItem implements Serializable {
+
+    private String date;
+    private String content;
+    private boolean done;
+
+    public ToDoItem(String content)
+    {
+        this.content = content;
+        this.date = new SimpleDateFormat("d.M.yyyy").format(new Date());    //nastaveni formatu dat
+        this.done = false;
+
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done)   //setter na done
+    {
+        this.done = done;
+    }
+
+    public void setContent(String aValue) {
+        content = aValue;
+    }
+}
--- src/ui/ProFrame.java
+++ src/ui/ProFrame.java
@@ -1,12 +1,20 @@
 package ui;
 
+import model.TableModel;
+import model.ToDoItem;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.List;
 
 public class ProFrame extends JFrame {      //rozsireni o JFrame
 
     static int width = 800;
     static int height = 600;
+    private TableModel model;
 
     public static void main(String... args) {
         ProFrame proFrame = new ProFrame();//vytvoreni instance (JFrame)
@@ -26,8 +34,57 @@ public class ProFrame extends JFrame {      //rozsireni o JFrame
         button.setText("Přidat poznámku");
         toolbar.add(button);
 
-        JTable table = new JTable();
+        JButton loadbutton = new JButton();
+        loadbutton.setText("Načíst data");
+        toolbar.add(loadbutton);
+
+        JButton savebutton = new JButton();
+        savebutton.setText("Uložit data");
+        toolbar.add(savebutton);
+
+        button.addActionListener(action -> {
+            ToDoItem item = new ToDoItem("Test obsah");
+            model.add(item);
+        });
+
+        loadbutton.addActionListener(action -> {
+            loadItems();
+        });
+
+        savebutton.addActionListener(action -> {
+            saveItems();
+        });
+
+        model = new TableModel();
+        JTable table = new JTable(model);
         add(new JScrollPane(table), BorderLayout.CENTER);     //zajisteni scrollingu u tabulky
         pack();
+
+        setLocationRelativeTo(null);        //neni komponenta, ke ktere by se priblizil - otevreni na stredu obrazovky
+    }
+
+    private void saveItems() {
+        try {
+            ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(new File("our.db")));
+            stream.writeObject(model.getItems());
+            stream.flush();
+            stream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadItems()
+    {
+        try
+        {
+            ObjectInputStream stream = new ObjectInputStream(new FileInputStream(new File("our.db")));
+            List<ToDoItem> items = (List<ToDoItem>) stream.readObject();
+            stream.close();
+            model.setItems(items);
+            model.fireTableDataChanged();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
